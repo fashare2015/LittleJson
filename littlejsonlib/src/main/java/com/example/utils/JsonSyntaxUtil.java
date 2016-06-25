60eecdf6ac22b4842cad818adc5d4e0f99286458
apple <153614131@qq.com>
2016-06-25 17:38:46 +0800

    改为流式

--- littlejsonlib/src/main/java/com/example/utils/JsonSyntaxUtil.java
+++ littlejsonlib/src/main/java/com/example/utils/JsonSyntaxUtil.java
@@ -3,9 +3,10 @@ package com.example.utils;
 import com.example.beans.JsonItem;
 import com.example.constant.JsonSyntax;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * User: fashare(153614131@qq.com)
@@ -29,43 +30,54 @@ public class JsonSyntaxUtil {
 //        return tmpStr.charAt(tmpStr.length() - 1) == JsonSyntax.BRACE_END;
 //    }
 
-    private static boolean checkItems(String tmpStr) {
-        String itemMap = tmpStr.substring(1, tmpStr.length()-1);
-        String[] items = itemMap.split(JsonSyntax.SEPARATOR + "");
+//    private static boolean checkItems(String tmpStr) {
+//        String itemMap = tmpStr.substring(1, tmpStr.length()-1);
+//        String[] items = itemMap.split(JsonSyntax.SEPARATOR + "");
+//
+//        for (String item: items) {
+//            // item => "\"key\": value"
+//            String[] keyAndValue = item.split(JsonSyntax.COLON + "");
+////            System.out.println(item);
+//
+//            if(keyAndValue.length != 2) return false;
+//            String key = keyAndValue[0].trim(), value = keyAndValue[1].trim();
+////            System.out.println(key + " => " + value);
+////            System.out.println(isString(key) + ", " + isString(value));
+//            if(!isString(key) || !isString(value))  return false;
+//        }
+//        return true;
+//    }
+//
+//    private static boolean isString(String string) {
+//        int len = string.length();
+//        return len >= 2
+//            && string.substring(0, 1).equals("\"")
+//            && string.substring(len-1, len).equals("\"");
+//    }
 
-        for (String item: items) {
-            // item => "\"key\": value"
-            String[] keyAndValue = item.split(JsonSyntax.COLON + "");
-//            System.out.println(item);
-
-            if(keyAndValue.length != 2) return false;
-            String key = keyAndValue[0].trim(), value = keyAndValue[1].trim();
-//            System.out.println(key + " => " + value);
-//            System.out.println(isString(key) + ", " + isString(value));
-            if(!isString(key) || !isString(value))  return false;
-        }
-        return true;
-    }
-
-    private static boolean isString(String string) {
-        int len = string.length();
-        return len >= 2
-            && string.substring(0, 1).equals("\"")
-            && string.substring(len-1, len).equals("\"");
-    }
+//    public static List<JsonItem> getItems(String jsonString) {
+//        String itemMap = getInnerString(jsonString);
+//        String[] items = itemMap.split(JsonSyntax.SEPARATOR + "");
+//        List<JsonItem> itemList = new ArrayList<>();
+//        for (String item: items) {  // item => "\"key\": value"
+//            String[] keyAndValue = item.split(JsonSyntax.COLON + "");
+//            itemList.add(new JsonItem(getInnerString(keyAndValue[0]), getInnerString(keyAndValue[1])));
+//        }
+//        return itemList;
+//    }
 
     public static List<JsonItem> getItems(String jsonString) {
-        String itemMap = getInnerString(jsonString);
-        String[] items = itemMap.split(JsonSyntax.SEPARATOR + "");
-        List<JsonItem> itemList = new ArrayList<>();
-        for (String item: items) {  // item => "\"key\": value"
-            String[] keyAndValue = item.split(JsonSyntax.COLON + "");
-            itemList.add(new JsonItem(getInnerString(keyAndValue[0]), getInnerString(keyAndValue[1])));
-        }
-        return itemList;
+        return Stream.of(jsonString)
+                .flatMap(str -> Stream.of(getInnerString(str))) // 去掉花括号
+                .flatMap(itemMap -> Stream.of(itemMap.split(JsonSyntax.SEPARATOR + "")))    // 按逗号分割
+                .flatMap(item -> {  // 将 String 转换为 JsonItem
+                    String[] keyAndValue = item.split(JsonSyntax.COLON + "");   // item => "\"key\": value".按冒号分割
+                    return Stream.of(new JsonItem(getInnerString(keyAndValue[0]), getInnerString(keyAndValue[1])));
+                })
+                .collect(Collectors.toList());  // 从 stream 中收集数据,返回一个 List<JsonItem>
     }
 
-    private static String getInnerString(String string){
+    private static String getInnerString(String string){    // 提取冒号中的实体
         return string.trim().substring(1, string.length()-1);
     }
 
